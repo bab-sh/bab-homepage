88c6f5e8d82ce6d44d906017fe92a4bc3cb4f3a9
AIO <Sebastian-Stepper@gmx.de>
2025-10-04 03:39:06 +0200

    resetup vite and tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,3 @@
 <template>
-    <NavbarComponent/>
-  <main class="main-content">
-  <router-view  />
-    </main>
-    <FooterComponent />
-
-
+  <div>test</div>
 </template>
-
-<script setup>
-import NavbarComponent from "@/components/common/NavbarComponent.vue";
-import FooterComponent from "@/components/common/FooterComponent.vue";
-</script>
-
-<style>
-@import 'tailwindcss/base';
-@import 'tailwindcss/components';
-@import 'tailwindcss/utilities';
-
-
-
-body {
-  font-family: 'Space Grotesk', sans-serif;
-  color: #e6e6e6;
-  @apply bg-background;
-}
-html, body {
-  height: 100%;
-  margin: 0;
-}
-
-main.main-content {
-  flex: 1;
-}
-
-</style>
